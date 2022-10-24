e83d198161c6f44b3af7f6820d2c80b8ac0e04fe
BLADEHEDA <93664026+BLADEHEDA@users.noreply.github.com>
2022-10-24 21:20:18 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,61 @@
 let buttons = document.querySelectorAll(".buttons");
-let tipinput=document.querySelector("#input1").value;
-let custominput= document.querySelector("#input2").value;
-let peopleinput = document.querySelector("#input3").value;
+let tipinput=document.querySelector("#input1");
+let custominput= document.querySelector("#input2");
+let peopleinput = document.querySelector("#input3");
 let resetbtn= document.querySelector(".reset");
 let tipamtperson = document.querySelector(".tip");
 let totalamtperson = document.querySelector(".total-amt");
-
+let discount=0; let amt=0; let ppl=0;
 
 // this functionsruns same for every button click 
 
-buttons.forEach(function(i){
-    i.addEventListener("click" , function(){
+reset()
+buttons.forEach(btn =>{
+    btn.addEventListener("click" ,()=>{
         // this stores the value of a button in a variable (then iconver it to percentege)
-  let value = (this.value)/100;
-  bill = (tipinput) *value;
-    console.log( value);
-    return value;
-    // calculate()
+        discount = btn.value;
+        custominput.value-"";         
+        calc()
     })
 })
 
-// function calculate() {
-     
+// the lines below decribes the behavior of the  people input field
+peopleinput.addEventListener("input" , function(){
+    ppl= peopleinput.value;
+    calc()
+})
+// the function describes the behavior of the custom input field 
+custominput.addEventListener("input" , function(){
+    let custom= custominput.value;
+    if(custom>0){
+        discount= custom;
+    }
+    calc()
+})
 
-//     bill = (tipinput) *value;
-//     console.log(bill)
-//  }
+// the lines below displays the behavior  of the bill input
+tipinput,addEventListener("input", ()=>{
+    amt=Number(tipinput.value);
+    calc()
+})
+// the lines below display the behaviour of the reset button 
+resetbtn.addEventListener("click", reset)
+    function reset(){
+    tipinput.value= "";
+    peopleinput.value=" ";
+    custominput.value=" ";
+    tipamtperson.textContent=0;
+    totalamtperson.innerHTML=0;
+
+} 
+
+// the function below performs the required calcultions 
+function calc(){
+    if(amt>=0 && ppl>=1){
+        let totalTip = (discount*amt)/(100);
+        let totalAmt = amt + totalTip;
+        tipamtperson.textContent= (totalTip/ppl).toFixed(2);
+        totalamtperson.innerHTML= (totalAmt/ppl).toFixed(2);
+    }
+ ;
+}
